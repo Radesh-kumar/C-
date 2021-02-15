2ee6d1bc20d8a7690cfa99256fd2b962cbe84175
Radesh-kumar <imradesh@gmail.com>
2021-02-15 14:53:40 +0530

    Dynamic Allocation

--- 1. Coding Ninjas/02.Dynamic Allocation/const_keyword.cpp
+++ 1. Coding Ninjas/02.Dynamic Allocation/const_keyword.cpp
@@ -2,37 +2,39 @@
 using namespace std;
 
 int main() {
+    // We have to initialize and assign values same time while using const (same as in case of reference)
+    const int i = 10;
+
     // 1.Reference
     int x = 10;
     int const &ref = x;
 
     cout << " " << endl;
-    cout << x++ << endl;
+    cout << "x = " <<x++ << endl;
     // we can change x value
     // ref++; (Not possible)  // we cant change value of x from reference
     cout << " " << endl;
 
-
-    const int i = 10;
-    // We have to initialize and assign values same time (same as in case of reference)
-
-    // 1.Pointers : 
-    // Case:1 constant integer and constantant pointing integer
+    // 2.Pointers : 
+    // Case:1  constant pointing integer
     int j = 20;
+    int const *p = &j;
     // const int *p == int const *p;	// pointer to const int
-    int *p = &j;
-    // j++;  //Value change for constant is not possible
-    cout << j << endl;
-     cout << " " << endl;
+    cout<< "j = " << ++j << endl;
     // (*p)++;  //Value change for constant is not possible
 
-    *p++;
-    cout << p << endl;
-    (*p)++;
-    cout << p << endl;
+    // Case:2  constant pointer
+    int k = 10;
+    int * const pk = &k;
+
     cout << " " << endl;
+    cout<< "k = " << ++k << endl;
+    cout<< "k = " << (*pk)++ << endl;
+    // value change is possible
+    // *pk++; we cant change the address of the pointer
 
 
 
+    
 
 }
